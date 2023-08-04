446161c3fafeac8588cee52d395b86f46dd9cc93
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-04 17:56:07 +0100

    basic math operations

--- src/math.java
+++ src/math.java
@@ -1,5 +1,13 @@
 public class math {
     public static void main(String args[]) {
+        double a = 3;
+        double b = 6 + a;
+        double c = 12;
+        double d = c - b;
+
+
+
+        System.out.println( d * c / b - a);
 
     }
 }
