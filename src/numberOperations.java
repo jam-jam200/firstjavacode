deba88020c1c57800ace27eaee6827ef357166d7
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-04 19:26:59 +0100

    logical and comparison operators

---
+++ src/numberOperations.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class numberOperations {
+    public static void main(String args[]) {
+        double a = 1.23;
+        double b = 6;
+        double c = 6;
+        double d = 4;
+
+        // comparison operators
+        boolean isEqual = a == b;
+        boolean isNotEqual = b != c;
+
+        // logical operators
+        boolean isTrue = b > d && c != a;
+
+        System.out.println(isNotEqual);
+
+
+
+    }
+}
