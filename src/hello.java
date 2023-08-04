4242283bbdf44ffeb20bd6b9b2a9eb4059faf13c
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-04 16:13:51 +0100

    data types

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,14 @@
 public class hello {
     public static void main (String args[]) {
-        System.out.println("Hello jessica");
+//        System.out.println("Hello jessica");
+
+//        data types
+        int var1 = 20;
+        char var2 = 'a';
+        String myName = "Jessica";
+        boolean isDark = false;
+
+        System.out.println("Hi, my name is " + myName + ". I am " + isDark);
+
     }
 }
