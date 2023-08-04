c78abc13012ac3f128f2992129386c45a027af02
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-04 20:37:25 +0100

    if statements

--- src/ifStatements.java
+++ src/ifStatements.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class ifStatements {
     public static void main(String[] args) {
-        System.out.println("Enter three numbers: ");
+        System.out.println("Enter two numbers: ");
         Scanner scanner = new Scanner(System.in);
         double num1 = scanner.nextDouble();
         double num2 = scanner.nextDouble();
@@ -10,9 +10,14 @@ public class ifStatements {
         if (num1 > num2){
             System.out.println("Num1 is greater than num 2");
         }
+
+        else if(num1 == num2) {
+            System.out.println("Num1 is equal to num2");
+        }
         else {
             System.out.println("Sorry, num2 is greater than num1");
         }
 
+        scanner.close();
     }
 }
