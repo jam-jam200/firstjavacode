2c43f2de62aaa4f3c8881ec47aea963fb5081c6a
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-10 00:14:58 +0100

    arrays

--- src/ArraysF.java
+++ src/ArraysF.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class ArraysF {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in 5 numbers: \n");
+        int data = scanner.nextInt();
+
+
+
+
         double[] num = new double[4];
         num[0] = 1;
         num[1] = 2;
         num[2] = 3;
         num[3] = 4.65;
 
-        System.out.println(num[3] + num[2]);
+//        System.out.println(num[3] + num[2]);
     }
 }
