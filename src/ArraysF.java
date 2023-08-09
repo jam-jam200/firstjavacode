5c191b5c7eb09fe9dd3d4356981658692ba0211a
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-09 23:58:06 +0100

    arrays

--- src/ArraysF.java
+++ src/ArraysF.java
@@ -1,5 +1,8 @@
 public class ArraysF {
     public static void main(String[] args) {
-
+        double[] num = new double[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[3] = 3;
     }
 }
