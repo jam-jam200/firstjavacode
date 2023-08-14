07bd5edba1eb792b45ce1871f95e4d3feb5f1838
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-14 23:38:47 +0100

    get users input and turing it to an array

--- src/ArraysF.java
+++ src/ArraysF.java
@@ -6,12 +6,12 @@ public class ArraysF {
         System.out.println("Enter 5 numbers");
         int[] data = new int[5];
 
-        for (int i = 0; i <= data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             System.out.println("Enter 5 numbers: " + (i + 1) + " : ");
             data[i] = scanner.nextInt();
         }
-        System.out.println(data);
-//        i'm back people
+
+
 
     }
 }
