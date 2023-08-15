e71037d808188fe6cdf0c3bfc52866a54cbb9843
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-15 17:35:36 +0100

    multiplication table using loops

--- src/loops.java
+++ src/loops.java
@@ -1,7 +1,10 @@
 public class loops {
     public static void main(String[] args){
-        for(int i = 100; i > 10; i--){
-            System.out.println(i);
+        for(int i = 0; i <= 10; i++){
+            int var = i;
+            for(int j = 1; j<=12; i++){
+                System.out.println();
+            }
         }
     }
 }
