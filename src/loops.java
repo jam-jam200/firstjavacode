fb55d810c576a172a5055328fa5fd41bc3f1f634
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-15 16:48:20 +0100

    starting loops

--- src/loops.java
+++ src/loops.java
@@ -1,6 +1,6 @@
 public class loops {
     public static void main(String[] args){
-        for(int i = 100; i <= 1000; i--){
+        for(int i = 100; i > 10; i--){
             System.out.println(i);
         }
     }
