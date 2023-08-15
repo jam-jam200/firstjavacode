66327f831f9d68a3c13f4c295fd295252bb7532e
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-15 16:51:48 +0100

    testing out strings

--- src/ArraysF.java
+++ src/ArraysF.java
@@ -4,6 +4,12 @@ public class ArraysF {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] data = new int[5];
+        String[] words = new String[3];
+
+        for (int w = 0; w < words.length; w++) {
+            System.out.println("Enter 5 words: " + (w + 1) + " : ");
+            words[w] = scanner.next();
+        }
 
         for (int i = 0; i < data.length; i++) {
             System.out.println("Enter 5 numbers: " + (i + 1) + " : ");
@@ -13,6 +19,11 @@ public class ArraysF {
         System.out.println("You have entered the following numbers: ");
         for (int number : data) {
             System.out.println(number + 2);
+        }
+
+        System.out.println("You have entered the following numbers: ");
+        for (String word : words) {
+            System.out.println(word);
         }
 
         scanner.close();
