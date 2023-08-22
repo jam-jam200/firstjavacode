a0143bfc6c75154d0ca21dce0bb5646006d0a7d4
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-22 21:57:28 +0100

    multiplication table using loops

--- src/loops.java
+++ src/loops.java
@@ -4,7 +4,7 @@ public class loops {
             System.out.println(i);
             for(int j = 1; j<=10; j++){
                 System.out.println("Multiplication of "+ i + " X " + j + " = " + i * j);
-                System.out.println("Addition of "+ i + " + " + j + " = " + i + j);
+                System.out.println("Addition of "+ i + " + " + j + " = " + (i + j));
                 System.out.println("Subtraction of "+ i + " - " + j + " = " + (i - j));
                 System.out.println("Division of "+ i + " / " + j + " = " + i / j);
             }
