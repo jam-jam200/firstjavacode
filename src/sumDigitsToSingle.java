d2731e5dfdaabc27515bee822f3e773020f42d43
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-18 18:40:00 +0100

    sum digits till single

--- src/sumDigitsToSingle.java
+++ src/sumDigitsToSingle.java
@@ -2,6 +2,12 @@ public class sumDigitsToSingle {
     public static void main(String[] args){
         int num = 23;
 
+        if (num < 10) {
+           System.out.println(num);
+        }
+
+
+
     }
 
 }
