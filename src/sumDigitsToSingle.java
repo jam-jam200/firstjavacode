4e19062ddfa3cf06d49050bc36f58164d18d4a65
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-20 15:59:09 +0100

    sum digits till single

--- src/sumDigitsToSingle.java
+++ src/sumDigitsToSingle.java
@@ -1,11 +1,24 @@
 public class sumDigitsToSingle {
     public static void main(String[] args){
         int num = 9875;
-        int result = 0;
+        int result = sumDigits(num);
         System.out.println("results is " + result);
 
     }
 
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            System.out.println(number);
+        }
+
+        String stringNum = String.valueOf(number);
+        int sum = 0;
+        for (int i = 0; i < stringNum.length(); i++) {
+            sum += Character.getNumericValue(stringNum.charAt(i));
+        }
+
+        return number;
+    }
 
 
 }
