26a40f53894fc9f03c93bcbbae815fc27dfdc9a1
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-20 16:42:24 +0100

    new car selector

---
+++ src/NewCarSelector.java
@@ -0,0 +1,14 @@
+public class NewCarSelector {
+    public static void main(String[] args) {
+        String[] carData = new String[5];
+        carData[0] = "White";
+        carData[1] = "Red";
+        carData[2] = "Blue";
+        carData[3] = "blue";
+        carData[4] = "Purple";
+
+        for(int i = 0; i < carData.length; i++) {
+
+        }
+    }
+}
--- src/newCarSelector.java
+++
@@ -1,5 +0,0 @@
-public class newCarSelector {
-    public static void main(String[] args) {
-//loop
-    }
-}
