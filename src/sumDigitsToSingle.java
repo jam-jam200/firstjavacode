6cb9e3aa7d6b4b04d03d0f0e4b994f48ccd90594
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-22 21:16:11 +0100

    new car selector

--- src/sumDigitsToSingle.java
+++ src/sumDigitsToSingle.java
@@ -2,7 +2,7 @@ public class sumDigitsToSingle {
     public static void main(String[] args) {
         int num = 1143;
         int result = sumDigits(num);
-        System.out.println("Result is " + result);
+        System.out.println("Result is of " + result);
     }
 
     public static int sumDigits(int number) {
