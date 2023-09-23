855436b408f7f7c59f3206745580d4da8177ada5
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-23 14:43:22 +0100

    grade converter

--- src/GradeConverter.java
+++ src/GradeConverter.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 public class GradeConverter {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.println("Enter the total number of courses taken this semester ");
+        Scanner scanner = new Scanner(System.in);
+    }
 
 }
