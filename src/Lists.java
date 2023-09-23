4a5752361165aed6196f5a6d609e1cc750cdd6d8
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-23 18:59:10 +0100

    basic test question

--- src/Lists.java
+++ src/Lists.java
@@ -3,7 +3,14 @@ public class Lists {
     public static void main(String[] args) {
         ArrayList<String> fruits = new ArrayList<>();
 
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Cucumber");
+        fruits.add("Strawberry");
+
         int fruitSize = fruits.size();
-        System.out.println(fruitSize);
+        System.out.println("fruit size is: " + fruitSize);
+        System.out.println(fruits.get(3));
     }
 }
