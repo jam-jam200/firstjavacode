c286a28b56d079d64d459ec33508c653328df487
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-23 17:56:49 +0100

    basic test question

--- src/BasicTest.java
+++ src/BasicTest.java
@@ -3,5 +3,18 @@ public class BasicTest {
     public static void main(String[] args){
         System.out.print("Enter name: ");
         Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println(name + " you are welcome to the test. Test starts now...");
+
+        System.out.println("Question 1: What is the capital city of Nigeria? ");
+        String answ1 = scanner.next();
+
+        while (!answ1.equals("Abuja")) {
+            System.out.println("Answer is incorrect! Try again...");
+            answ1 = scanner.next();
+        }
+
+        System.out.println("Good answer!");
+        System.out.println("Question 1: What is my favourite fruit? ");
     }
 }
