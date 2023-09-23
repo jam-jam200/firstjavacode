a8585ce3d4cc451acc74bfd91ad85627db160242
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-23 18:48:38 +0100

    basic test question

--- src/Lists.java
+++ src/Lists.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
 public class Lists {
     public static void main(String[] args) {
-    //
+        ArrayList<String> fruits = new ArrayList<>();
+
+        int fruitSize = fruits.size();
+        System.out.println(fruitSize);
     }
 }
