0f82f6a5d155b028729745a357f5ad88c93017ce
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-23 16:17:37 +0100

    grade converter

--- src/GradeConverter.java
+++ src/GradeConverter.java
@@ -13,7 +13,7 @@ public class GradeConverter {
         for(int i = 0; i< numCourses.length; i++) {
             //looping through so user can enter scores and names of courses
             for (int j = 0; j < courseName.length; j++) {
-                System.out.println("Enter name of course " + (i + 1) + ":");
+                System.out.println("Enter name of course: ");
                 courseName[j] = scanner.next();
             System.out.println("Enter score for " + courseName[j] + ":");
                 numCourses[i] = scanner.nextDouble();
