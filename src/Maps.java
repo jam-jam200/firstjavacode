5ff1eb4fb803affbdb85b94127fd2858d630c410
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-27 22:13:32 +0100

    maps

--- src/Maps.java
+++ src/Maps.java
@@ -2,6 +2,12 @@ import java.util.Map;
 import java.util.HashMap;
 public class Maps {
     public static void main(String[] args) {
-
+        Map colorPaint = new HashMap();
+        colorPaint.put("Blue", 2);
+        colorPaint.put("Purple", 3);
+        colorPaint.put("Pink", 5);
+        colorPaint.put("Yellow", 7);
+        colorPaint.put("Green", 1);
+        colorPaint.put("White", 4);
     }
 }
