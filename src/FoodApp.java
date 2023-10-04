65822a4cff1bf1425c355c0ecdc00deb2a7f3945
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-04 13:48:43 +0100

    food app with objects and classes

--- src/FoodApp.java
+++ src/FoodApp.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class FoodApp {
     public static void main(String[] args) {
+        System.out.println("Welcome to My Mini food App❤️! \n To proceed you would need to create a password. \n Type yes to continue or no to exit program ");
+
 
     }
 }
