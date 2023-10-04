14f846566d25f2ac91caae809ec91fb6a71e79d3
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-04 14:46:14 +0100

    food app with objects and classes

--- src/FoodApp.java
+++ src/FoodApp.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 
 public class FoodApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to My Mini food App❤️! \n To proceed you would need to create a password. \n Type yes to continue or no to exit program ");
+        System.out.println("Welcome to My Mini food App❤️! \nTo proceed you would need to create a password. \nType yes to continue or no to exit program ");
         Scanner scanner = new Scanner(System.in);
         String ans = scanner.next();
 
         if( ans.equalsIgnoreCase("yes")) {
-            System.out.println("Thank you please create password: ");
+            System.out.println("Thank you please create password☺️☺️: ");
         } else {
             System.exit(0);
         }
+
+        String pass1 = scanner.next();
+        System.out.println("Your password is: " + pass1);
+
+
 
     }
 }
