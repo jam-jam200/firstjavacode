1eeb996c160b40669432392ffa35ad7ce08dd76f
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-04 12:08:22 +0100

    maps

--- src/Maps.java
+++ src/Maps.java
@@ -3,12 +3,15 @@ import java.util.HashMap;
 public class Maps {
     public static void main(String[] args) {
         Map colorPaint = new HashMap();
+        //the one in "" is the key while the one outside is the value
         colorPaint.put("Blue", 2);
         colorPaint.put("Purple", 3);
         colorPaint.put("Pink", 5);
         colorPaint.put("Yellow", 7);
         colorPaint.put("Green", 1);
         colorPaint.put("White", 4);
+
+        colorPaint.remove("Pink");
 
 
         System.out.println(colorPaint);
