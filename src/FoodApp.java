342f98379fd67d9a2d3c8a1aafe5643b51fe252a
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-04 16:31:50 +0100

    food app with objects and classes

--- src/FoodApp.java
+++ src/FoodApp.java
@@ -19,18 +19,17 @@ public class FoodApp {
         String ans2 = scanner.next();
 
         if (ans2.equalsIgnoreCase("yes")) {
-            System.out.println("We have Rice, Swallow and Fried Rice. WHat would you like among those? ");
+            System.out.println("We have rice, garri and beans. What would you like among those? ");
             String ans3 = scanner.next();
 
-            while(!ans3.equalsIgnoreCase("Rice") || !ans3.equalsIgnoreCase("Swallow") || !ans3.equalsIgnoreCase("Fried Rice")) {
+            while(!ans3.equalsIgnoreCase("rice") && !ans3.equalsIgnoreCase("beans") && !ans3.equalsIgnoreCase("garri")) {
                 System.out.println("Sorry😓, You have to choose from the list. Try again!");
                 ans3 = scanner.next();
-            }
-            if(ans3.equalsIgnoreCase("Rice") || ans3.equalsIgnoreCase("Swallow") || ans3.equalsIgnoreCase("Fried Rice")) {
+            } if(ans3.equalsIgnoreCase("rice") && ans3.equalsIgnoreCase("beans") && ans3.equalsIgnoreCase("garri")) {
                 System.out.println("Would you like a drink with that?: ");
                 String ans4 = scanner.next();
 
-                while (!ans4.equalsIgnoreCase("yes") || !ans4.equalsIgnoreCase("no")) {
+                while (!ans4.equalsIgnoreCase("yes") && !ans4.equalsIgnoreCase("no")) {
                     System.out.println("wrong command!");
                     ans4 = scanner.next();
                 }
@@ -39,12 +38,12 @@ public class FoodApp {
                     System.out.println("Would you like Sprite or Juice");
                     String ans5 = scanner.next();
 
-                    while (!ans5.equalsIgnoreCase("Sprite") || !ans5.equalsIgnoreCase("Juice")) {
+                    while (!ans5.equalsIgnoreCase("Sprite") && !ans5.equalsIgnoreCase("Juice")) {
                         System.out.println("Sorry😓, You have to choose from the list. Try again!");
                         ans5 = scanner.next();
                     }
 
-                    if(ans5.equalsIgnoreCase("Sprite") || ans5.equalsIgnoreCase("Juice")) {
+                    if(ans5.equalsIgnoreCase("Sprite") && ans5.equalsIgnoreCase("Juice")) {
                         System.out.println("You have ordered " + ans3 + " with a bottle of " + ans5 + "\n To proceed enter password again: ");
 
                         String pass2 = scanner.next();
